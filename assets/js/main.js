351e5941e6005f0610cd7a1dbc763be665d0e315
agnescameron <agnesfcameron@gmail.com>
2019-07-29 21:16:41 +0300

    added chatroulette button

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ function closeLetter() {
 
 
 function placeDivs() {
-    for(var i=0; i<11; i++){
+    for(var i=0; i<20; i++){
         var left = Math.random()*($(window).width() - 450) + 50;
         var top = Math.random()*($(window).height() - 450) + 50;
         $( `#draggable${i}`).css({'left': left, 'top': top});
@@ -17,7 +17,6 @@ function placeDivs() {
 function changeCursor() {
     $('#zach').attr(`src`, `assets/img/zach-heads/zach${clicks%5 + 1}.png`);
     clicks = clicks+1;
-    console.log(clicks%7)
     var sound = document.getElementById("audio");
     sound.play();
 }
@@ -34,6 +33,15 @@ function changeCursor() {
     $( "#draggable8").draggable();
     $( "#draggable9").draggable();
     $( "#draggable10").draggable();
+    $( "#draggable11").draggable();
+    $( "#draggable12").draggable();
+    $( "#draggable13").draggable();
+    $( "#draggable14").draggable();
+    $( "#draggable15").draggable();
+    $( "#draggable16").draggable();
+    $( "#draggable17").draggable();
+    $( "#draggable18").draggable();
+    $( "#draggable19").draggable();    
   } );
 
 placeDivs();
