35a6f43afa2a4a6014d6e7f68a9f988eb821a0e2
agnescameron <agnesfcameron@gmail.com>
2019-07-29 20:30:47 +0300

    audio works, added images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
+var clicks = 1;
+
+function closeLetter() {
+    $('.letterBox').hide()
+}
 
 
 function placeDivs() {
     for(var i=0; i<11; i++){
-        var left = Math.random()*($(window).width() - 250) + 50;
-        var top = Math.random()*($(window).height() - 250) + 50;
+        var left = Math.random()*($(window).width() - 450) + 50;
+        var top = Math.random()*($(window).height() - 450) + 50;
         $( `#draggable${i}`).css({'left': left, 'top': top});
         $( `#draggable${i}`).show();
     }
+}
+
+function changeCursor() {
+    $('#zach').attr(`src`, `assets/img/zach-heads/zach${clicks%5 + 1}.png`);
+    clicks = clicks+1;
+    console.log(clicks%7)
+    var sound = document.getElementById("audio");
+    sound.play();
 }
 
   $( function() {
@@ -24,3 +37,5 @@ function placeDivs() {
   } );
 
 placeDivs();
+
+$(document).on("click", function() { changeCursor() });
